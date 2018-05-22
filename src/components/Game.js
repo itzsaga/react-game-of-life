a36cbf032041039a123b136ed6272798d6ece079
Seth <itz.saga@gmail.com>
2018-05-21 22:26:27 -0500

    Add getElementOffset

--- src/components/Game.js
+++ src/components/Game.js
@@ -38,6 +38,17 @@ class Game extends Component {
     }
     return cells
   }
+
+  getElementOffset() {
+    const rect = this.boardRef.getBoundingClientRect()
+    const doc = document.documentElement
+
+    return {
+      x: (rect.left + window.pageXOffset) - doc.clientLeft,
+      y: (rect.top + window.pageYOffset) - doc.clientTop,
+    }
+  }
+
   render() {
     return (
       <Fragment>
