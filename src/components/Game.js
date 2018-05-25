5b31a6349f05ac2e5d11e9943686a465de9577a8
Seth <itz.saga@gmail.com>
2018-05-24 22:07:06 -0500

    Calculate those neighbors

--- src/components/Game.js
+++ src/components/Game.js
@@ -101,6 +101,22 @@ class Game extends Component {
     }, this.state.interval)
   }
 
+  calculateNeighbors(board, x, y) {
+    let neighbors = 0
+    const dirs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
+    for (let i = 0; i < dirs; i++) {
+      const dir = dirs[i]
+      let y1 = y + dir[0]
+      let x1 = x + dir[1]
+
+      if (x1 >= 0 && x1 < this.cols && y1 >= 0 && y1 < this.rows && board[y1][x1]) {
+        neighbors++
+      }
+    }
+
+    return neighbors
+  }
+
   handleIntervalChange = e => {
     this.setState({
       interval: e.target.value
